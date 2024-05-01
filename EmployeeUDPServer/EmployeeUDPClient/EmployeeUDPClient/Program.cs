a302a24a690fe3abcce514251d7cfa5ea6d41ad6
lisisisan <125748777+lisisisan@users.noreply.github.com>
2024-05-01 13:05:08 +0500

    lab1-Server

---
+++ EmployeeUDPServer/EmployeeUDPClient/EmployeeUDPClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class EmployeeUDPClient
+{
+    public static void Main(string[] args)
+    {
+        // Устанавливаем IP-адрес и порт сервера по умолчанию
+        string ipAddress = "127.0.0.1";
+        Int32 port = 13000;
+
+        // Если IP-адрес передан как параметр командной строки, используем его
+        if (args.Length > 0)
+            ipAddress = args[0];
+
+        // Создаем экземпляр класса UdpClient для отправки и получения данных по UDP
+        UdpClient udpClient = new UdpClient(ipAddress, port);
+        
+        // Создаем объект IPEndPoint для хранения информации о удаленном хосте
+        IPEndPoint remoteEndPoint = null;
+
+        // Бесконечный цикл для взаимодействия с пользователем
+        while (true)
+        {
+            // Запрашиваем имя сотрудника у пользователя
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            
+            // Если пользователь ввел пустую строку, выходим из цикла
+            if (name == "") 
+                break;
+            
+            // Преобразуем имя сотрудника в массив байт для отправки по сети
+            byte[] sendData = Encoding.ASCII.GetBytes(name);
+            
+            // Отправляем данные на сервер
+            udpClient.Send(sendData, sendData.Length);
+            
+            // Принимаем данные от сервера и сохраняем информацию об удаленном хосте
+            byte[] receiveData = udpClient.Receive(ref remoteEndPoint);
+            
+            // Преобразуем полученные данные в строку и выводим на консоль
+            string job = Encoding.ASCII.GetString(receiveData);
+            Console.WriteLine(job);
+        }
+    }
+}
---
+++ EmployeeUDPServer/EmployeeUDPServer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+class EmployeeUDPServer
+{
+    // Наш "эмулятор базы данных"
+    // Словарь, в котором мы будем хранить всю информацию
+    static Dictionary<string, string> employees =
+        new Dictionary<string, string>()
+    {
+        {"john", "manager"},
+        {"jane", "steno"},
+        {"jim", "clerk"},
+        {"jack", "salesman"}
+    };
+
+    public static void Main()
+    {
+        // Устанавливаем порт, на котором будет работать сервер
+        Int32 port = 13000;
+        
+        // Создаем экземпляр класса UdpClient для прослушивания входящих сообщений по протоколу UDP
+        UdpClient udpc = new UdpClient(port);
+        
+        // Выводим сообщение о запуске сервера и используемом порту
+        Console.WriteLine("Server started, servicing on port " + port.ToString());
+        
+        // Создаем объект IPEndPoint для хранения информации о удаленном хосте
+        IPEndPoint? ep = null;
+        
+        // Бесконечный цикл для обработки входящих сообщений
+        while (true)
+        {
+            // Принимаем данные от клиента и сохраняем информацию о удаленном хосте
+            byte[] receiveData = udpc.Receive(ref ep);
+            
+            // Преобразуем полученные байты в строку (имя сотрудника)
+            string name = Encoding.ASCII.GetString(receiveData);
+            
+            // Получаем должность сотрудника из словаря
+            string job = employees[name];
+            
+            // Если сотрудника с таким именем нет, возвращаем сообщение "No such employee"
+            if (job == null) 
+                job = "No such employee";
+            
+            // Преобразуем ответ в байтовый массив
+            byte[] sendData = Encoding.ASCII.GetBytes(job);
+            
+            // Отправляем ответ клиенту по UDP
+            udpc.Send(sendData, sendData.Length, ep);
+        }
+    }
+}
